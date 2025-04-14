b8bb3d4bae22d4a8752a740852b8a1ce959ede7f
JoseP055 <jospaba12@gmail.com>
2025-04-13 20:00:23 -0600

    Add MNIST dataset loader and preprocessing script to extract x_train, y_train, x_test, y_test

---
+++ src/proccesMNIST.py
@@ -0,0 +1,56 @@
+import os
+import numpy as np
+
+mnist_path = './data/MNIST/'
+
+def list_files(mnist_path):
+    return [os.path.join(mnist_path, f) for f in os.listdir(mnist_path) if os.path.isfile(os.path.join(mnist_path, f))]
+
+def get_images(mnist_path):
+    for f in list_files(mnist_path):
+        if 'train-images' in f:
+            with open(f, 'rb') as data:
+                _ = int.from_bytes(data.read(4), 'big')
+                num_images = int.from_bytes(data.read(4), 'big')
+                rows = int.from_bytes(data.read(4), 'big')
+                cols = int.from_bytes(data.read(4), 'big')
+                train_images = data.read()
+                x_train = np.frombuffer(train_images, dtype=np.uint8).reshape((num_images, rows, cols))
+        elif 'train-labels' in f:
+            with open(f, 'rb') as data:
+                data.read(8)  # skip header
+                y_train = np.frombuffer(data.read(), dtype=np.uint8)
+        elif 't10k-images' in f:
+            with open(f, 'rb') as data:
+                _ = int.from_bytes(data.read(4), 'big')
+                num_images = int.from_bytes(data.read(4), 'big')
+                rows = int.from_bytes(data.read(4), 'big')
+                cols = int.from_bytes(data.read(4), 'big')
+                test_images = data.read()
+                x_test = np.frombuffer(test_images, dtype=np.uint8).reshape((num_images, rows, cols))
+        elif 't10k-labels' in f:
+            with open(f, 'rb') as data:
+                data.read(8)
+                y_test = np.frombuffer(data.read(), dtype=np.uint8)
+
+    return x_train, y_train, x_test, y_test
+
+x_train, y_train, x_test, y_test = get_images(mnist_path)
+
+print(f"x_train shape: {x_train.shape}")
+print(f"y_train shape: {y_train.shape}")
+print(f"x_test shape: {x_test.shape}")
+print(f"y_test shape: {y_test.shape}")
+
+import matplotlib.pyplot as plt
+
+def show_number(x, y=None):
+    plt.imshow(x, cmap='gray')
+    if y is not None:
+        plt.title(f'Dígito: {y}')
+    plt.axis('off')
+    plt.show()
+
+for i in range(5):
+    rand_idx = np.random.randint(len(y_test))
+    show_number(x_test[rand_idx], y_test[rand_idx])
