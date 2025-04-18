5117fbf46f8d075a82e3a84e4a381ac8cc131598
JoseP055 <jospaba12@gmail.com>
2025-04-17 19:36:18 -0600

    Implement core neural network functions: forward pass, ReLU, softmax, cross-entropy loss, and backpropagation

--- src/neural_network.py
+++ src/neural_network.py
@@ -57,3 +57,69 @@ parameters = init_parameters(28*28, [200, 10])
 # print(parameters["b1"].shape)
 # print(parameters["W2"].shape)
 # print(parameters["b2"].shape)
+
+# FUNCTIONS
+
+#RELU
+def relu(x):
+    return np.maximum(0, x)
+
+#Scores
+def scores(x, paremeters, activation_fnc):
+    """
+    x has (#pixels, num samples) shape
+    """
+    z1 = parameters["W1"] @ x + parameters["b1"]
+    a1 = activation_fnc(z1)
+    z2 = parameters["W2"] @ a1 + parameters["b2"]
+
+    return z2, z1, a1
+
+scores, z1, a1 = scores(x_train[:64].T, parameters, relu)
+# print(x_train[:64].T.shape) # (784, 64)
+
+# Softmax
+def softmax(x):
+    exp_scores= np.exp(x)
+    sum_exp_scores = np.sum(exp_scores, axis=0)
+    probs = exp_scores/sum_exp_scores
+
+    return probs
+
+# Loss Entropy
+
+def x_entropy(scores, y, batch_size=64):
+    probs = softmax(scores)
+    y_hat = probs[y.squeeze(), np.arange(batch_size)]
+    cost = np.sum(-np.log(y_hat)) / batch_size
+
+    return probs, cost
+
+# Back Propagation // Backward
+def backward(probs, x, y, z1, a1, parameters, batch_size=64):
+    grads = {}
+    probs[y.squeeze(), np.arange(batch_size)] -= 1 # y_hat - y
+    dz2 = probs.copy()
+
+    dW2 = dz2 @ a1.T / batch_size
+    db2 = np.sum(dz2, axis=1, keepdims=True) / batch_size
+    da1 = parameters['W2'].T @ dz2
+
+    dz1 = da1.copy()
+    dz1[z1 <= 0] = 0
+
+    dW1 = dz1 @ x 
+    db1 = np.sum(dz1, axis=1, keepdims=True)
+
+    assert parameters['W1'].shape == dW1.shape, "W1 not same shape"
+    assert parameters['W2'].shape == dW2.shape, "W2 not same shape"
+    assert parameters['b1'].shape == db1.shape, "b1 not same shape"
+    assert parameters['b2'].shape == db2.shape, "b2 not same shape"
+
+    grads = {'W1': dW1, "b1":db1, "W2": dW2, "b2": db2}
+    
+    return grads
+
+y_hat, cost = x_entropy(scores, y_train[:64])
+#print(y_hat.shape)
+grads = backward(y_hat, x_train[:64], y_train[:64], z1, a1, parameters)
